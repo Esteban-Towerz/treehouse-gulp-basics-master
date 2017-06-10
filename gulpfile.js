a87fd8c556f25687c6e3d5ae45bb0474a655d464
Esteban Torres <esteban.towerz@gmail.com>
2017-06-09 19:23:21 -0500

    Added some descriptive comments and create other gulp tasks with their respective dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// the convention is use the name of the module, when the module names contain a - so you have to pick good names and obvious names like concat
 var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
@@ -8,18 +8,20 @@ var gulp = require('gulp'),
     maps = require('gulp-sourcemaps'),
      del = require('del');
 
+// 1. Concatenating Scripts in one app.js file
 gulp.task("concatScripts", function() {
-    return gulp.src([
+    return gulp.src([ //putting the return statement is because to each task know when the task is finish and start it.
         'js/jquery.js',
         'js/sticky/jquery.sticky.js',
         'js/main.js'
         ])
     .pipe(maps.init())
     .pipe(concat('app.js'))
-    .pipe(maps.write('./'))
+    .pipe(maps.write('./')) // .pipe(maps.write('../maps')) 
     .pipe(gulp.dest('js'));
 });
 
+// 2. Minification of our Javascirpt files
 gulp.task("minifyScripts", ["concatScripts"], function() {
   return gulp.src("js/app.js")
     .pipe(uglify())
@@ -31,12 +33,12 @@ gulp.task('compileSass', function() {
   return gulp.src("scss/application.scss")
       .pipe(maps.init())
       .pipe(sass())
-      .pipe(maps.write('./'))
+      .pipe(maps.write('./')) // the current working directory relative to the gulp.dest directory
       .pipe(gulp.dest('css'));
 });
 
 gulp.task('watchFiles', function() {
-  gulp.watch('scss/**/*.scss', ['compileSass']);
+  gulp.watch('scss/**/*.scss' , ['compileSass']); // gulb has something call globing pattern is simple a syntaz to matching a names of files like 'scss/**/*.scss' that means looking the subfolder looking the subdirectories and any file with the sass extension .scss
   gulp.watch('js/main.js', ['concatScripts']);
 })
 
@@ -46,12 +48,13 @@ gulp.task('clean', function() {
 
 gulp.task("build", ['minifyScripts', 'compileSass'], function() {
   return gulp.src(["css/application.css", "js/app.min.js", 'index.html',
-                   "img/**", "fonts/**"], { base: './'})
+                   "img/**", "fonts/**"], { base: './'}) //i need to provide the gulp.source method with a parameter with the base option, it tells to gulp to keep the directories structure of everything that's provided in the source 
             .pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', ['watchFiles']);
 
 gulp.task("default", ["clean"], function() {
-  gulp.start('build');
+  gulp.start('build'); // the build task will run once the clean task has finish
 });
+//the gulp.start method is going to change in gulp 4
